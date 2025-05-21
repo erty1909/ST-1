efc289c374eb905a83f9fc007cb01c08ef02af05
Aleksandra Matyunina <ERTY-00@yandex.ru>
2025-05-21 22:36:34 +0300

    st-1

--- test/tests.cpp
+++ test/tests.cpp
@@ -5,13 +5,69 @@
 #include "alg.h"
 
 
-TEST(st1, sumPrime1) {
-  uint64_t res = sumPrime(2000000);
-  uint64_t expected = 142913828922;
-  EXPECT_EQ(expected, res);
+TEST(PrimeTest, CheckPrimeBasic) {
+    EXPECT_TRUE(checkPrime(2));
+    EXPECT_TRUE(checkPrime(3));
+    EXPECT_TRUE(checkPrime(5));
+    EXPECT_TRUE(checkPrime(7));
+    EXPECT_TRUE(checkPrime(11));
+    EXPECT_TRUE(checkPrime(13));
+    EXPECT_TRUE(checkPrime(17));
 }
-TEST(st1, sumPrime2) {
-  uint64_t res = sumPrime(10);
-  uint64_t expected = 17;
-  EXPECT_EQ(expected, res);
+
+TEST(PrimeTest, CheckPrimeNonPrime) {
+    EXPECT_FALSE(checkPrime(1));
+    EXPECT_FALSE(checkPrime(4));
+    EXPECT_FALSE(checkPrime(6));
+    EXPECT_FALSE(checkPrime(8));
+    EXPECT_FALSE(checkPrime(9));
+    EXPECT_FALSE(checkPrime(10));
+    EXPECT_FALSE(checkPrime(12));
+}
+
+TEST(PrimeTest, CheckPrimeLarge) {
+    EXPECT_TRUE(checkPrime(97));
+    EXPECT_TRUE(checkPrime(101));
+    EXPECT_FALSE(checkPrime(100));
+    EXPECT_FALSE(checkPrime(102));
+}
+
+
+TEST(PrimeTest, NPrimeBasic) {
+    EXPECT_EQ(nPrime(1), 2);
+    EXPECT_EQ(nPrime(2), 3);
+    EXPECT_EQ(nPrime(3), 5);
+    EXPECT_EQ(nPrime(4), 7);
+    EXPECT_EQ(nPrime(5), 11);
+}
+
+TEST(PrimeTest, NPrimeZero) {
+    EXPECT_EQ(nPrime(0), 0);
+}
+
+
+TEST(PrimeTest, NextPrimeBasic) {
+    EXPECT_EQ(nextPrime(2), 3);
+    EXPECT_EQ(nextPrime(3), 5);
+    EXPECT_EQ(nextPrime(4), 5);
+    EXPECT_EQ(nextPrime(5), 7);
+    EXPECT_EQ(nextPrime(6), 7);
+}
+
+TEST(PrimeTest, NextPrimeEdgeCases) {
+    EXPECT_EQ(nextPrime(0), 2);
+    EXPECT_EQ(nextPrime(1), 2);
+}
+
+
+TEST(PrimeTest, SumPrimeBasic) {
+    EXPECT_EQ(sumPrime(3), 2);
+    EXPECT_EQ(sumPrime(5), 5);
+    EXPECT_EQ(sumPrime(10), 17);
+}
+
+TEST(PrimeTest, SumPrimeZeroOne) {
+    EXPECT_EQ(sumPrime(0), 0);
+    EXPECT_EQ(sumPrime(1), 0);
+    EXPECT_EQ(sumPrime(2), 0);
 }
